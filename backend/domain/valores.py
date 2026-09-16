67c7935406a05c61b684c1cc5c3feb4a20260e5c
pincho <julian.devgp@gmail.com>
2026-09-15 23:41:28 -0500

    domain add
    se añadio valores.py, Rango,EstadoParametro; EstadoPlanta

---
+++ backend/domain/valores.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+class EstadoParametro(Enum):
+    BAJO = "BAJO"
+    OPTIMO = "OPTIMO"
+    ALTO = "ALTO"
+
+class EstadoPlanta(Enum):
+    SALUDABLE = "SALUDABLE"
+    EN_RIESGO = "EN_RIESGO"
+    CRITICO = "CRITICO"
+
+class Rango():
+    def __init__(self, minimo, maximo):
+        if minimo > maximo:
+            raise ValueError("El valor mínimo no puede ser mayor que el valor máximo.")
+        self.minimo = minimo
+        self.maximo = maximo
+        
+
+def clasificar(self, valor):
+    if valor < self.minimo:
+        return EstadoParametro.BAJO
+    elif valor > self.maximo:
+        return EstadoParametro.ALTO
+    else:
+        return EstadoParametro.OPTIMO
+        
